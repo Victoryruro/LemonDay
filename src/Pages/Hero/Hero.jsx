a665f0a49edbf621342337c295eb694f82829f86
Victory Diemuodeke <Victorydiemuodeke@gmail.com>
2026-07-01 16:15:28 +0100

    Done with hero and Footer

--- src/Pages/Hero/Hero.jsx
+++ src/Pages/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from 'motion/react'
 import "./Hero.css"
-import image from "../../assets/LemonHero 2.png"
+import image from "../../assets/LemonHero.png"
 
 const Hero = () => {
   return (
@@ -9,14 +9,14 @@ const Hero = () => {
         <div className=" overflow-hidden">
             <section className="relative py-16 sm:py-24 lg:py-32">
                 <div className="absolute inset-0 z-0">
-                <div className="h-full w-full bg-cover bg-center opacity-20" id='Hero-back' ></div>
-                <div className="absolute inset-0 bg-white opacity-50"></div>
+                <div className="h-full w-full bg-cover bg-center opacity-50" id='Hero-back' ></div>
+                <div className="absolute inset-0 bg-white opacity-20"></div>
                 </div>
                 <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 relative z-10">
-                <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2">
-                    <div className="text-white">
+                <div className="grid items-center grid-cols-1 gap-12 lg:grid-cols-2 h-[60vh]">
+                    <div className="text-gray-950 text-center lg:text-left">
                     <motion.p
-                      className="text-base font-semibold tracking-wider text-lime-400 uppercase"
+                      className="text-base  font-semibold tracking-wider text-lime-700 uppercase"
                       initial={{ opacity: 0, filter: 'blur(20px)', y: 20 }}
                       animate={{ opacity: 1, filter: 'blur(0px)', y: 0 }}
                       transition={{ duration: 0.8, ease: 'easeInOut', delay: 0 }}
@@ -25,16 +25,16 @@ const Hero = () => {
 
                     </motion.p>
                     <motion.h1
-                      className="mt-4 text-4xl font-bold lg:mt-8 sm:text-6xl xl:text-7xl"
+                      className="mt-4 text-5xl font-bold lg:mt-8 sm:text-7xl xl:text-8xl text-nowrap"
                       initial={{ opacity: 0, filter: 'blur(20px)', y: 20 }}
                       animate={{ opacity: 1, filter: 'blur(0px)', y: 0 }}
                       transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.3 }}
                     >
-                        Premium Lemonade Served
+                        Premium Lemonade <br /> Served
 
                     </motion.h1>
 <motion.p
-                  className="mt-4 text-base lg:mt-8 sm:text-xl text-gray-300"
+                  className="mt-4 text-lg lg:mt-8 sm:text-xl text-gray-700 font-medium"
                   initial={{ opacity: 0, filter: 'blur(20px)', y: 20 }}
                   animate={{ opacity: 1, filter: 'blur(0px)', y: 0 }}
                   transition={{ duration: 0.8, ease: 'easeInOut', delay: 0.6 }}
@@ -48,7 +48,7 @@ const Hero = () => {
 <motion.a
                   href="#discover"
                   title="Discover your next journey"
-                  className="inline-flex items-center px-8 py-4 mt-8 font-semibold text-white transition-all duration-300 bg-lime-600 rounded-full hover:bg-lime-700 focus:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+                  className="inline-flex items-center px-8 py-4 mt-8 font-semibold text-gray-200 transition-all duration-300 bg-lime-700 rounded-full hover:bg-citrus focus:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-gray-900"
                   role="button"
                   aria-label="Discover your next journey"
                   initial={{ opacity: 0, filter: 'blur(20px)', y: 20 }}
@@ -62,7 +62,7 @@ const Hero = () => {
                 </motion.a>
 
                     <motion.p
-                      className="mt-6 text-gray-400"
+                      className="mt-6 text-mint"
                       initial={{ opacity: 0, filter: 'blur(20px)', y: 20 }}
                       animate={{ opacity: 1, filter: 'blur(0px)', y: 0 }}
                       transition={{ duration: 0.8, ease: 'easeInOut', delay: 1.2 }}
@@ -72,9 +72,9 @@ const Hero = () => {
                     </motion.p>
                     </div>
 
-                    <div className="relative hidden lg:block">
+                    <div className="relative hidden lg:block ">
                     <motion.img
-                      className="w-full h-auto "
+                      className="w-full h-auto scale-150 mt-[20vh] "
                       src={image}
                       alt="Futuristic travel image with a planet and starry sky"
                       initial={{ opacity: 0, filter: 'blur(10px)', y: 20 }}
